2bb6b54f23e6b18a188dd15a4b8c99c0de38c4f0
fengyecsg <850379974@qq.com>
2017-01-13 16:07:35 +0800

    自定义文件

---
+++ CHObject.h
@@ -0,0 +1,15 @@
+//
+//  CHObject.h
+//  AppDelegate
+//
+//  Created by shugang.chang on 17/1/13.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHObject : NSObject
+
+- (void)chlog;
+
+@end
---
+++ CHObject.m
@@ -0,0 +1,17 @@
+//
+//  CHObject.m
+//  AppDelegate
+//
+//  Created by shugang.chang on 17/1/13.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#import "CHObject.h"
+
+@implementation CHObject
+
+- (void)chlog {
+    
+}
+
+@end
